2c943053478700fce1446d62066bced4d6cc9234
Lester <lester.weijers@gmail.com>
2023-01-14 23:41:53 +0100

    Update installatie.sh

--- installatie.sh
+++ installatie.sh
@@ -45,9 +45,9 @@ systemctl restart apache2
 
 #Destroy systemd-resolved
 echo "Deactivating systemd-resolved"
-systemctl disable systemd-resolved
+#systemctl disable systemd-resolved
 systemctl stop systemd-resolved
-systemctl mask systemd-resolved
+#systemctl mask systemd-resolved
 
 
 #hostapd.conf
@@ -104,7 +104,7 @@ echo "Creating the rc.local config..."
 touch /etc/rc.local
 touch /etc/systemd/system/rc-local.service
 printf '%s\n' '[Unit]' 'Description=/etc/rc.local Compatibillity' 'ConditionPathExists=/etc/rc.local' '' '[Service]' 'Type=forking' 'ExecStart=/etc/rc.local start' 'TimeoutSec=0' 'StandardOutput=tty' 'RemainAfterExit=yes' 'SysVStartPriority=99' '' '[Install]' 'WantedBy=multi-user.target' | sudo tee /etc/systemd/system/rc-local.service
-printf '%s\n' '#!/bin/bash' 'iptables-restore < /etc/iptables.ipv4.nat' 'netplan apply' 'sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"' 'exit 0' | sudo tee /etc/rc.local
+printf '%s\n' '#!/bin/bash' 'iptables-restore < /etc/iptables.ipv4.nat' 'netplan apply' 'sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"' 'systemctl stop systemd-resolved' 'systemctl start dnsmasq' 'exit 0' | sudo tee /etc/rc.local
 chmod +x /etc/rc.local
 systemctl unmask rc-local
 systemctl enable rc-local
