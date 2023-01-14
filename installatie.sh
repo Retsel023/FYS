7fd8c181d03463326d71bbcc83f87d6a2b38c797
Lester <lester.weijers@gmail.com>
2023-01-15 00:45:47 +0100

    Update installatie.sh

--- installatie.sh
+++ installatie.sh
@@ -43,10 +43,14 @@ echo "creating the certificate..."
 openssl req -x509 -nodes -days 36500 -newkey rsa:2048 -keyout /var/www/server.key -out /var/www/server.crt -subj "/C=NL/ST=North-Holland/L=Amsterdam/CN=127.0.1.1"
 systemctl restart apache2
 
+#unbinding systemctl-resolved from port 53
+printf '%s\n' '[Resolve]' 'DNSStubListener=no' | sudo tee /etc/systemd/resolved.conf
+systemctl restart systemd-resolved
+
 #Destroy systemd-resolved
-echo "Deactivating systemd-resolved"
-systemctl stop systemd-resolved
-systemctl mask systemd-resolved
+#echo "Deactivating systemd-resolved"
+#systemctl stop systemd-resolved
+#systemctl mask systemd-resolved
 
 
 #hostapd.conf
